1d09298e52bf98a10dc05e0ef813c1c9ba305155
Marko Durdev <marko.durdev@erasys.de>
2024-12-02 17:08:58 +0100

    Add firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notes/business/product_bloc.dart';
@@ -8,8 +9,13 @@ import 'package:flutter_notes/ui/model/note_ui_model.dart';
 import 'business/note_bloc.dart';
 import 'common/widgets/note_loading_indicator.dart';
 import 'data/notes_repository.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const NoteApp());
 }
 
