f0eeab41dcb40b8f24b411c598871e0baeb13535
Marko Durdev <marko.durdev@erasys.de>
2024-12-02 10:47:53 +0100

    Implement mapping of the response

--- lib/data/model/product_model.dart
+++ lib/data/model/product_model.dart
@@ -1,23 +1,44 @@
-class ProductModelResponse {
+class Product {
   final String id;
   final String name;
+  final ProductData? data;
 
-  const ProductModelResponse({
-    required this.id,
-    required this.name,
-  });
+  Product({required this.id, required this.name, this.data});
 
-  factory ProductModelResponse.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'id': String id,
-        'name': String name,
-      } =>
-        ProductModelResponse(
-          id: id,
-          name: name,
-        ),
-      _ => throw const FormatException('Failed to load album.'),
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      data: json['data'] != null ? ProductData.fromJson(json['data']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'data': data?.toJson(),
+    };
+  }
+}
+
+class ProductData {
+  final String? color;
+  final String? capacity;
+
+  ProductData({this.color, this.capacity});
+
+  factory ProductData.fromJson(Map<String, dynamic> json) {
+    return ProductData(
+      color: json['color'] as String?,
+      capacity: json['capacity'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'color': color,
+      'capacity': capacity,
     };
   }
 }
--- lib/data/remote/product_repository.dart
+++ lib/data/remote/product_repository.dart
@@ -16,11 +16,23 @@ class ProductRepositoryImpl extends ProductRepository {
           await http.get(Uri.parse('https://api.restful-api.dev/objects'));
 
       if (response.statusCode == 200) {
-        // If the server did return a 200 OK response,
-        // then parse the JSON.
-        var something = ProductModelResponse.fromJson(
-            jsonDecode(response.body) as Map<String, dynamic>);
-        print("I received sometihing $something");
+        List<dynamic> jsonList = jsonDecode(response.body);
+
+        print("json list is $jsonList");
+
+        // Parse each item in the list into a Product object
+        List<Product> products = jsonList.map((item) => Product.fromJson(item)).toList();
+
+
+        print("products $products");
+
+        // Print parsed objects
+        for (var product in products) {
+          print('ID: ${product.id}');
+          print('Name: ${product.name}');
+          print('Color: ${product.data?.color}');
+          print('Capacity: ${product.data?.capacity}');
+        }
       } else {
         // If the server did not return a 200 OK response,
         // then throw an exception.
