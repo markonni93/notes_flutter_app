0d6c2d563ce54030e01224335ff8f77fe633d8c6
marko <marko.djurdjev93@gmail.com>
2024-12-03 12:07:50 +0100

    Implement new navigation bar and make getChildForIndex method

--- lib/ui/main_widget.dart
+++ lib/ui/main_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_notes/main.dart';
 import 'package:flutter_notes/ui/home/home_widget.dart';
 import 'package:flutter_notes/ui/main_bloc.dart';
 import 'package:flutter_notes/ui/settings/settings_widget.dart';
@@ -24,30 +25,39 @@ class MainWidget extends StatelessWidget {
                             child: child,
                           );
                         },
-                        child: state.currentIndex == 0
-                            ? const HomeWidget()
-                            : const SettingsWidget(),
+                        child: _getWidgetForIndex(state.currentIndex),
                       ),
-                      bottomNavigationBar: BottomNavigationBar(
-                          currentIndex: state.currentIndex,
-                          onTap: (index) => context
+                      bottomNavigationBar: NavigationBar(
+                          selectedIndex: state.currentIndex,
+                          onDestinationSelected: (index) => context
                               .read<MainWidgetBloc>()
                               .add(TabClickedEvent(index: index)),
-                          items: MainBottomBarITem.values.map((navItem) {
-                            return BottomNavigationBarItem(
-                              icon: Icon(navItem.icon),
-                              label: navItem.title,
-                            );
-                          }).toList())));
+                          destinations: MainBottomBarItem.values
+                              .map((e) => NavigationDestination(
+                                    icon: Icon(e.icon),
+                                    label: e.title,
+                                  ))
+                              .toList())));
             }));
   }
 }
 
-enum MainBottomBarITem {
+Widget _getWidgetForIndex(int currentIndex) {
+  switch (currentIndex) {
+    case 0:
+      return const HomeWidget();
+    case 1:
+      return const SettingsWidget();
+    default:
+      throw Exception("something went wrong");
+  }
+}
+
+enum MainBottomBarItem {
   home(title: "Home", icon: Icons.home),
   settings(title: "Settings", icon: Icons.settings);
 
-  const MainBottomBarITem({required this.title, required this.icon});
+  const MainBottomBarItem({required this.title, required this.icon});
 
   final String title;
   final IconData icon;
