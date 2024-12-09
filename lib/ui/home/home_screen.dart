5ff70391934052b0ffb830deedaa9a57e028a188
Marko Durdev <marko.durdev@erasys.de>
2024-12-09 20:49:23 +0100

    Properly implement dark scrim background on home screen when fab is opened

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -34,76 +34,69 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        physics: const BouncingScrollPhysics(),
-        slivers: <Widget>[
-          SliverAppBar(
-              stretch: true,
-              stretchTriggerOffset: 300.0,
-              pinned: true,
-              expandedHeight: 200.0,
-              flexibleSpace: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-                  // Calculate the collapse percentage
-                  var top = constraints.biggest.height;
-                  bool isCollapsed = top <=
-                      kToolbarHeight + MediaQuery.of(context).padding.top;
+      body: Stack(children: [
+        CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: <Widget>[
+            SliverAppBar(
+                stretch: true,
+                stretchTriggerOffset: 300.0,
+                pinned: true,
+                expandedHeight: 200.0,
+                flexibleSpace: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    // Calculate the collapse percentage
+                    var top = constraints.biggest.height;
+                    bool isCollapsed = top <=
+                        kToolbarHeight + MediaQuery.of(context).padding.top;
 
-                  return FlexibleSpaceBar(
-                    centerTitle: true,
-                    title: isCollapsed
-                        ? const Text("Quick Notes") // Show when collapsed
-                        : null, // Hide when expanded
-                    background: Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: SvgPicture.asset(notebook1)),
+                    return FlexibleSpaceBar(
+                      centerTitle: true,
+                      title: isCollapsed
+                          ? const Text("Quick Notes") // Show when collapsed
+                          : null, // Hide when expanded
+                      background: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: SvgPicture.asset(notebook1)),
+                    );
+                  },
+                ),
+                actions: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.settings),
+                    tooltip: 'Add new entry',
+                    onPressed: () {
+                      /* ... */
+                    },
+                  )
+                ]),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    color: index.isOdd ? Colors.white : Colors.black12,
+                    height: 100.0,
+                    child: Center(
+                      child: Text('$index',
+                          textScaler: const TextScaler.linear(5.0)),
+                    ),
                   );
                 },
+                childCount: 20,
               ),
-
-              // FlexibleSpaceBar(
-              //   background: Padding(padding: const EdgeInsets.all(16) , child: SvgPicture.asset(notebook1)),
-              // ),
-              actions: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.settings),
-                  tooltip: 'Add new entry',
-                  onPressed: () {
-                    /* ... */
-                  },
-                )
-              ]),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  color: index.isOdd ? Colors.white : Colors.black12,
-                  height: 100.0,
-                  child: Center(
-                    child: Text('$index',
-                        textScaler: const TextScaler.linear(5.0)),
-                  ),
-                );
-              },
-              childCount: 20,
             ),
+          ],
+        ),
+        Positioned.fill(
+            child: IgnorePointer(
+          ignoring: !_fabOpened,
+          child: AnimatedOpacity(
+            opacity: _fabOpened ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 250),
+            child: Container(color: Colors.black26),
           ),
-        ],
-      ),
-
-      // Stack(children: [
-      //   Positioned.fill(
-      //     child: AnimatedOpacity(
-      //       opacity: _fabOpened ? 1.0 : 0.0,
-      //       duration: const Duration(milliseconds: 250),
-      //       child: Container(
-      //         color: Colors.black26,
-      //         width: MediaQuery.of(context).size.width,
-      //         height: MediaQuery.of(context).size.height,
-      //       ),
-      //     ),
-      //   )
-      // ]),
+        ))
+      ]),
       floatingActionButton: ExpandableFab(
         distance: 112,
         children: [
